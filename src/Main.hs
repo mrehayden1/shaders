61adb3c2bd90313426382c91a8895736e2494ca6
Matthew Hayden <mrehayden@gmail.com>
2024-12-17 19:17:01 +0000

    Renaming and support for local logging changes.

--- src/Main.hs
+++ src/Main.hs
@@ -25,8 +25,12 @@ newtype LoggerT m a = LoggerT {
     unLoggerT :: ReaderT LogLevel m a
   } deriving (Functor, Applicative, Monad)
 
-runLogger :: LogLevel -> LoggerT m a -> m a
-runLogger l = flip runReaderT l . unLoggerT
+-- Local logging changes, useful for debugging.
+logger :: Monad m => LogLevel -> LoggerT m a -> LoggerT m a
+logger l = LoggerT . local (const l) . unLoggerT
+
+runLoggerT :: LogLevel -> LoggerT m a -> m a
+runLoggerT l = flip runReaderT l . unLoggerT
 
 instance MonadIO m => MonadIO (LoggerT m) where
   liftIO = LoggerT . liftIO
@@ -48,7 +52,7 @@ instance MonadIO m => MonadLogger (LoggerT m) where
 
 
 main :: IO ()
-main = runLogger appLoggingLevel $ do
+main = runLoggerT appLoggingLevel $ do
   liftIO $ printf "\nStarting %s v%s...\n\n" appName appVersion
   liftIO $ putStrLn "Copyright 2024 Categorical Industries.\n"
 
